368516af7f45aeef02c0d063722eeb9b943c7615
Dulmika <semaldulmika@gmail.com>
2024-02-09 14:20:54 +0530

    load book list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from './common/nav/nav.component';
+import { ViewAllBooksComponent } from './pages/view-all-books/view-all-books.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavComponent],
+  imports: [CommonModule, RouterOutlet, NavComponent, ViewAllBooksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/pages/view-all-books/view-all-books.component.ts
+++ src/app/pages/view-all-books/view-all-books.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-view-all-books',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, FormsModule, CommonModule],
   templateUrl: './view-all-books.component.html',
   styleUrl: './view-all-books.component.css'
 })
-export class ViewAllBooksComponent {
 
+export class ViewAllBooksComponent implements OnInit {
+  private http;
+
+  public bookList: any;
+
+
+
+  constructor(httpClient: HttpClient) {
+    this.http = httpClient;
+  }
+   
+
+  ngOnInit(): void {
+    this.loadBooksList();
+  }
+
+
+  loadBooksList() {
+    this.http.get("http://localhost:8080/book/get-all")
+      .subscribe((data) => {
+        this.bookList = data;
+      });
+  }
 }
